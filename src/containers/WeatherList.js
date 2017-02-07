7e1a94c33ace17809d03ddaea04900123e7062fc
omar.chacin <omar.chacin@sigmagroup.no>
2017-02-07 11:07:40 +0100

    add sparkline component and make a reusable chart

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/Chart';
+
 
 
 class WeatherList extends React.Component {
 
   renderWeather(cityData) {
-    console.log('cityData ', cityData);
-    const name = cityData.city.name;
+    const name  = cityData.city.name;
+    const temps = cityData.list.map((weather) => weather.main.temp);
+    const humidities = cityData.list.map((weather) => weather.main.humidity);
+    const pressures = cityData.list.map((weather) => weather.main.pressure);
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Chart data={temps} color="red"/>
+        </td>
+        <td>
+          <Chart data={pressures} color="yellow"/>
+        </td>
+        <td>
+          <Chart data={humidities} color="blue"/>
+        </td>
       </tr>
     )
   }
 
   render() {
-    console.log('this ', this.props);
     return (
       <table className="table table-hover">
         <thead>
